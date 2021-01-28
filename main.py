b1df30639c334bf4e4c8ab28f5e3502941b79c53
Chiara <chichibabe98@gmail.com>
2021-01-29 00:03:35 +1100

    todo list

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #python script to get weather info and create 'Walk Archie' events on google calendar
 
+#TODO - 6pm earliest walkies in afternoon, otherwise morning walk - 7-8am
+
+from datetime import datetime, timedelta
 import datetime
 from astral import LocationInfo
 from astral.sun import sun
 city = LocationInfo("Sydney", "Australia", "Australia/Sydney", -33.87, 151.21)
 
 s = sun(city.observer, date=datetime.date(2021, 1, 29), tzinfo = city.timezone)
-print((
-    f'Dawn:    {s["dawn"]}\n'
-    f'Sunrise: {s["sunrise"]}\n'
-    f'Noon:    {s["noon"]}\n'
-    f'Sunset:  {s["sunset"]}\n'
-    f'Dusk:    {s["dusk"]}\n'
-))
+
+walk_start = s["sunset"] - timedelta(hours=1)
+walk_end = s["sunset"]
+
+print("Start walkies: " + str(walk_start))
+print("End walkies: " + str(walk_end))
+#TODO - round to nearest 15 mins
+
